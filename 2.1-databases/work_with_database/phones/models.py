7cb9bee9866704b07246c2b01f575c487ed3d7b7
Ann Kh <anna.khrust@gmail.com>
2023-12-18 19:17:13 +0300

    homework ended

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    slug = models.SlugField(default=None, unique=True)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
