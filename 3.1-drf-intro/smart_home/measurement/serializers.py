4fd9f7bcb1005f8253e82a8163e9477ff1c45486
Ann Kh <anna.khrust@gmail.com>
2023-12-27 18:48:06 +0300

    homework completed

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -1,3 +1,21 @@
 from rest_framework import serializers
 
-# TODO: опишите необходимые сериализаторы
+from .models import Sensor, Measurement
+
+
+class MeasurementsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = '__all__'
+
+class SensorsListSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
+class SensorDetailSerializer(serializers.ModelSerializer):
+    measurements = MeasurementsListSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description', 'measurements']
