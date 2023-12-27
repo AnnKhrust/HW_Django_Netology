96f2fad663af167d95b858fa9c5b553c4c79653f
Ann Kh <anna.khrust@gmail.com>
2023-12-27 18:31:55 +0300

    missed task added

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,16 +5,17 @@ from .models import Student, Teacher
 
 
 def students_list(request):
+    object_students = Student.objects.all()
+    object_teachers = Teacher.objects.all()
+    students_count = [c.students.count() for c in object_teachers]
     template = 'school/students_list.html'
-    students = Student.objects.all().order_by('group')
-    teachers = Teacher.objects.all()
     context = {
-        'students': students,
-        'teachers': teachers
-    }
+        'object_students': object_students,
+        'pairs': zip(object_teachers, students_count),
+        }
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # ordering = 'group' # упорядочивание  настроено в models.py в модели Meta
 
     return render(request, template, context)
